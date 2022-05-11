5f73f7806f91a1559690ba142c22b606840be354
kdaud <dkakumirizii@gmail.com>
2022-05-11 20:20:48 +0300

    Sample out the widget

--- qaframework-bdd-tests/src/test/java/org/openmrs/contrib/qaframework/RunTest.java
+++ qaframework-bdd-tests/src/test/java/org/openmrs/contrib/qaframework/RunTest.java
@@ -17,7 +17,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 // defining ordered by name features folder
-features = {"src/test/resources/features"},
+features = { "src/test/resources/features" },
 // defining the definition steps package
 glue = "org.openmrs.contrib.qaframework.automation", plugin = {
 		"html:target/index.html", "message:target/cucumber.ndjson"}, monochrome = true)
